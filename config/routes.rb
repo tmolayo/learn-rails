612f7d05e58091f84154ff1bc502f9617e66e5d5
Tomide Molayo <molayo@gmail.com>
2015-07-13 03:49:01 +0000

    contact form

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-   post 'contact', to: 'contacts#process_form'
+  # post 'contact', to: 'contacts#process_form'
+  resources :contacts, only: [:new, :create]
    root to: 'visitors#new' 
 end
 
